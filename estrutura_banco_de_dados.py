79f85f5a90096d52e17a8326b39d863202cb4c12
RBeraldo84 <rodrigo.beraldo26@gmail.com>
2023-11-15 13:53:32 -0300

    segundo commit

--- estrutura_banco_de_dados.py
+++ estrutura_banco_de_dados.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # Criar um instância de SQLAlchemy
 app.config['SECRET_KEY'] = 'FSD2323f#$!SAH'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:EyqVnTaH0lVCXrtH@db.atpnbtsuhsehgrzpbffq.supabase.co:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pBVa3L5zdmsJSxLT@db.quadjpyivoaqzwnikxuy.supabase.co:5432/postgres'
 
 db = SQLAlchemy(app)
 db: SQLAlchemy
@@ -32,14 +32,14 @@ class Autor(db.Model):
 
 
 def inicializar_banco():
-    # Executar o comando para criar o banco de dados
-    db.drop_all()
-    db.create_all()
+   with app.app_context(): # Executar o comando para criar o banco de dados
+        db.drop_all()
+        db.create_all()
     # Criar usuários adminstradores
-    autor = Autor(nome='jhonatan', email='jhonatan@email.com',
+        autor = Autor(nome='jhonatan', email='jhonatan@email.com',
                   senha='123456', admin=True)
-    db.session.add(autor)
-    db.session.commit()
+        db.session.add(autor)
+        db.session.commit()
 
 
 if __name__ == "__main__":
